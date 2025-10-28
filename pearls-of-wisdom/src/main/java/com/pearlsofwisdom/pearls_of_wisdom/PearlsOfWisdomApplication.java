df6f1d8d4b5fb1a0eb88b015e6c1b6268c7b2443
Amany01 <67011395+Amany01@users.noreply.github.com>
2025-10-28 15:01:03 +0300

    Initial commitInitial commit: Spring Boot project setup

---
+++ pearls-of-wisdom/src/main/java/com/pearlsofwisdom/pearls_of_wisdom/PearlsOfWisdomApplication.java
@@ -0,0 +1,13 @@
+package com.pearlsofwisdom.pearls_of_wisdom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PearlsOfWisdomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PearlsOfWisdomApplication.class, args);
+	}
+
+}
---
+++ pearls-of-wisdom/src/test/java/com/pearlsofwisdom/pearls_of_wisdom/PearlsOfWisdomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pearlsofwisdom.pearls_of_wisdom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PearlsOfWisdomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
